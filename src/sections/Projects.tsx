c028515cce87188b575c70a6c43d5f22552da848
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-10-23 20:05:00 +0700

    Refactor: Update ProjectsSection styles for responsiveness in tablet view

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -54,17 +54,17 @@ export const ProjectsSection = () => {
             My Portofolio
           </p>
         </div>
-        <h2 className="font-serif text-3xl text-center mt-6">
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
           Featured Projects
         </h2>
-        <p className="text-center text-white/60 mt-4">
+        <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">
           See how I transformed concepts into engaging digital experiences.
         </p>
-        <div className="flex flex-col mt-10 gap-20">
+        <div className="mt-10 md:mt-20 flex flex-col gap-20">
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:-z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none">
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:-z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none">
               <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
                 <span>{project.company}</span>
                 <span>&bull;</span>
@@ -75,20 +75,22 @@ export const ProjectsSection = () => {
                 style={{
                   backgroundImage: `url(${grainImage.src})`,
                 }}></div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul className="flex flex-col gap-4 mt-4">
+              <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                {project.title}
+              </h3>
+              <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+              <ul className="flex flex-col gap-4 mt-4 md:mt-5">
                 {project.results.map((result) => (
                   <li
                     key={result.title}
-                    className="flex gap-2 text-sm text-white/50">
-                    <CheckIcon className="size-5" />
+                    className="flex gap-2 text-sm md:text-base text-white/50">
+                    <CheckIcon className="size-5 md:size-6" />
                     <span>{result.title}</span>
                   </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button className="bg-white text-gray-950 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
                   <span>Visit Live Site</span>
                   <ArrowRightIcon className="size-4" />
                 </button>
@@ -96,7 +98,7 @@ export const ProjectsSection = () => {
               <Image
                 src={project.image}
                 alt={project.title}
-                className="mt-8 -mb-4"
+                className="mt-8 -mb-4 md:-mb-0"
               />
             </div>
           ))}
