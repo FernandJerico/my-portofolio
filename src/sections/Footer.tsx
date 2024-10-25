c11158b2a8133d776324be71a4f516e22f5cf8a4
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-10-25 11:46:13 +0700

    Feat: Add Footer section to Home page

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,39 @@
+import ArrowRightIcon from "@/assets/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  {
+    title: "GitHub",
+    href: "#",
+  },
+  {
+    title: "LinkedIn",
+    href: "#",
+  },
+  {
+    title: "Instagram",
+    href: "#",
+  },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8">
+          <div className="text-white/40">&copy; 2024, All rights reserved.</div>
+          <nav className="flex flex-col items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                key={link.title}
+                href={link.href}
+                className="inline-flex items-center gap-1.5">
+                <span className="font-semibold">{link.title}</span>
+                <ArrowRightIcon className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
