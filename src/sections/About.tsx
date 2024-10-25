63d5f4c5d725a05b8667528adc1833124f27ac7e
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-10-25 11:08:47 +0700

    Refactor: Update CardHeader and Card components for responsiveness and styling consistency

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -24,14 +24,6 @@ import { ToolboxItem } from "@/components/ToolboxItem";
 
 const toolboxItems = [
   {
-    title: "Bootstrap",
-    iconType: Bootstrap,
-  },
-  {
-    title: "CSS3",
-    iconType: CSS3,
-  },
-  {
     title: "Dart",
     iconType: Dart,
   },
@@ -50,6 +42,14 @@ const toolboxItems = [
   {
     title: "Github",
     iconType: Github,
+  },
+  {
+    title: "Bootstrap",
+    iconType: Bootstrap,
+  },
+  {
+    title: "CSS3",
+    iconType: CSS3,
   },
   {
     title: "PHP",
@@ -72,12 +72,12 @@ const toolboxItems = [
     iconType: Javascript,
   },
   {
-    title: "MySQL",
-    iconType: Mysql,
-  },
-  {
     title: "Kotlin",
     iconType: Kotlin,
+  },
+  {
+    title: "MySQL",
+    iconType: Mysql,
   },
   {
     title: "React",
@@ -113,7 +113,7 @@ const hobbies = [
   {
     title: "Photography",
     emoji: "📷",
-    left: "70%",
+    left: "60%",
     top: "45%",
   },
   {
@@ -132,7 +132,7 @@ const hobbies = [
 
 export const AboutSection = () => {
   return (
-    <div className="py-20 ">
+    <div className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
@@ -140,17 +140,17 @@ export const AboutSection = () => {
           description="I'm a mobile developer with a passion for building beautiful and intuitive user interfaces. I specialize in Flutter and Kotlin"
         />
         <div className="mt-20 flex flex-col gap-8">
-          <div className="md:grid md:grid-cols-5 md:gap-8">
-            <Card className="h-[320px] md:col-span-2">
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
               <CardHeader
                 title="My Reads"
                 description="Explore the books shaping my perspectives."
               />
-              <div className="w-40 mx-auto mt-2">
+              <div className="w-40 mx-auto mt-2 md:mt-0">
                 <Image src={bookImage} alt="Book Image" />
               </div>
             </Card>
-            <Card className="h-[320px] md:col-span-3">
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
               <CardHeader
                 title="My Toolbox"
                 description="Explore the technologies and tools I use to craft exceptional
@@ -165,39 +165,41 @@ export const AboutSection = () => {
               />
             </Card>
           </div>
-          <Card className="h-[320px] p-0 flex flex-col">
-            <CardHeader
-              title="Beyond the Code"
-              description="Explore my interest and hobbies beyond the digital realmn."
-              className="px-6 pt-6"
-            />
-            <div className="relative flex-1">
-              {hobbies.map((hobby) => (
-                <div
-                  key={hobby.title}
-                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
-                  style={{
-                    left: hobby.left,
-                    top: hobby.top,
-                  }}>
-                  <span className="font-medium text-gray-950">
-                    {hobby.title}
-                  </span>
-                  <span>{hobby.emoji}</span>
-                </div>
-              ))}
-            </div>
-          </Card>
-          <Card className="h-[320px] p-0 relative">
-            <Image
-              src={mapImage}
-              alt="Map Image"
-              className="h-full w-full object-cover object-left-top"
-            />
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
-              <Image src={emojiSmile} alt="Emoji Smile" className="size-20" />
-            </div>
-          </Card>
+          <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyond the Code"
+                description="Explore my interest and hobbies beyond the digital realmn."
+                className="px-6 pt-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}>
+                    <span className="font-medium text-gray-950">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+              <Image
+                src={mapImage}
+                alt="Map Image"
+                className="h-full w-full object-cover object-left-top"
+              />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                <Image src={emojiSmile} alt="Emoji Smile" className="size-20" />
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
